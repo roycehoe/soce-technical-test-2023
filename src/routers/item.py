a96d7936153fa2e767312b7c63e2e60a9a68c1a3
roycehoe <royceinbox@hotmail.com>
2023-04-17 13:43:13 +0800

    feat: Create fully functioning get requests to retrieve individual, and all items

--- src/routers/item.py
+++ src/routers/item.py
@@ -24,13 +24,17 @@ USER_TO_UPDATE = {"name": "hello", "number": 1337, "is_happy": True}
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create(request: ItemIn, db: Session = Depends(get_db)):
     DataStore(db).create(models.Item(**request.dict()))
-    return "success"
+    return {
+        "message": "Item created"
+    }  # TODO: Create raising of errors for proper handling
 
 
 @router.post("/many", status_code=status.HTTP_201_CREATED)
 def create_many(request: list[ItemIn], db: Session = Depends(get_db)):
     DataStore(db).create_many([models.Item(**data.dict()) for data in request])
-    return "success"
+    return {
+        "message": "Items created"
+    }  # TODO: Create raising of errors for proper handling
 
 
 @router.get("/{item_id}", status_code=status.HTTP_200_OK, response_model=list[ItemOut])
@@ -38,12 +42,20 @@ def get(
     item_id: int,
     db: Session = Depends(get_db),
 ):
-    return DataStore(db).get(models.Item, filter={"id": item_id})
+    if item := DataStore(db).get(models.Item, filter={"id": item_id}):
+        return item
+    return {
+        "message": "No items found"
+    }  # TODO: Create raising of errors for proper handling
 
 
-@router.get("/all", status_code=status.HTTP_200_OK, response_model=list[ItemOut])
+@router.get("/all/", status_code=status.HTTP_200_OK, response_model=list[ItemOut])
 def get_all(db: Session = Depends(get_db)):
-    return DataStore(db).get_all(models.Item)
+    if all_items := DataStore(db).get_all(models.Item):
+        return all_items
+    return {
+        "message": "No items found"
+    }  # TODO: Create raising of errors for proper handling
 
 
 @router.get("/search", status_code=status.HTTP_200_OK, response_model=list[ItemOut])
@@ -56,13 +68,18 @@ def search(
     sort: Optional[SortOrder] = None,
     db: Session = Depends(get_db),
 ):
-    return DataStore(db).search(models.Item, field, q, limit, offset, sort_col, sort)
+    if searched_items := DataStore(db).search(
+        models.Item, field, q, limit, offset, sort_col, sort
+    ):
+        return searched_items
+    return {
+        "message": "No items found"
+    }  # TODO: Create raising of errors for proper handling
 
 
 @router.put("/{item_id}", status_code=status.HTTP_201_CREATED)
-def update(filter: dict, item_id: int, db: Session = Depends(get_db)):
-    if item := db.query(models.Item).filter(models.Item.id == item_id).first():
-        return DataStore(db).update(filter, models.Item, item)
+def update(item_id: int, updated_item: ItemIn, db: Session = Depends(get_db)):
+    return DataStore(db).update({"id": item_id}, models.Item, updated_item)
 
 
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
