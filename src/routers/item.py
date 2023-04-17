bc459e997acc5979713b333dee908d4a3ced94aa
roycehoe <royceinbox@hotmail.com>
2023-04-17 16:00:01 +0800

    feat: Create http exceptions for invalid filter requests

--- src/routers/item.py
+++ src/routers/item.py
@@ -13,21 +13,6 @@ SortOrder = Literal["asc", "desc"]
 router = APIRouter(tags=["Item"], prefix="/item")
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-def create(request: ItemIn, db: Session = Depends(get_db)):
-    if created_item := DataStore(db).create(models.Item(**request.dict())):
-        return created_item
-    raise HTTPException(status_code=422, detail=f"Faied to create item: {request}")
-
-
-@router.post("/many", status_code=status.HTTP_201_CREATED)
-def create_many(request: list[ItemIn], db: Session = Depends(get_db)):
-    DataStore(db).create_many([models.Item(**data.dict()) for data in request])
-    return {
-        "message": "Items created"
-    }  # TODO: Create raising of errors for proper handling
-
-
 @router.get("/{item_id}", status_code=status.HTTP_200_OK, response_model=ItemOut)
 def get(
     item_id: int,
@@ -38,6 +23,33 @@ def get(
     raise HTTPException(
         status_code=404, detail=f"Item with item_id: {item_id} not found"
     )
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create(request: ItemIn, db: Session = Depends(get_db)):
+    if created_item := DataStore(db).create(models.Item(**request.dict())):
+        return created_item
+    raise HTTPException(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        detail=f"Faied to create item: {request}",
+    )
+
+
+@router.post("/many", status_code=status.HTTP_201_CREATED)
+def create_many(request: list[ItemIn], db: Session = Depends(get_db)):
+    DataStore(db).create_many([models.Item(**data.dict()) for data in request])
+
+
+@router.put("/{item_id}", status_code=status.HTTP_201_CREATED)
+def update(item_id: int, updated_item: ItemIn, db: Session = Depends(get_db)):
+    return DataStore(db).update({"id": item_id}, models.Item, updated_item.dict())
+    # TODO: Create raising of errors for proper handling
+
+
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: str, db: Session = Depends(get_db)):
+    DataStore(db).delete(models.Item, filter={"id": id})
+    return {"message": "Item deleted"}
 
 
 @router.get("/all/", status_code=status.HTTP_200_OK, response_model=list[ItemOut])
@@ -64,15 +76,3 @@ def search(
     raise HTTPException(
         status_code=404, detail=f"No items for given search parameters found"
     )
-
-
-@router.put("/{item_id}", status_code=status.HTTP_201_CREATED)
-def update(item_id: int, updated_item: ItemIn, db: Session = Depends(get_db)):
-    return DataStore(db).update({"id": item_id}, models.Item, updated_item.dict())
-    # TODO: Create raising of errors for proper handling
-
-
-@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: str, db: Session = Depends(get_db)):
-    DataStore(db).delete(models.Item, filter={"id": id})
-    return {"message": "Item deleted"}
