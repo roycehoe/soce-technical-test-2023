499753d7b04cfec440ff4c4f922debe46287094f
roycehoe <royceinbox@hotmail.com>
2023-04-17 22:48:49 +0800

    feat: Create item card with hardcoded data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,63 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
+
+const PREPOPULATED_ITEMS = [
+  {
+    name: "Coke Zero",
+    quantity: 100,
+    price: 1.8,
+    description: "A healthier alternative to regular coke",
+  },
+  {
+    name: "Coke Regular",
+    quantity: 20,
+    price: 1.6,
+    description: "An unhealthier alternative to coke zero",
+  },
+  {
+    name: "Twisties",
+    quantity: 38,
+    price: 1.0,
+    description: "A tasty corn-based snack!",
+  },
+  {
+    name: "Liquorice",
+    quantity: 22,
+    price: 1.2,
+    description: "Just why.....",
+  },
+  {
+    name: "Top 10",
+    quantity: 8,
+    price: 1.5,
+    description: "The poor man's Mangum ice cream. Tastes just as great!",
+  },
+];
 </script>
 
 <template>
   <div class="w-screen h-screen flex flex-col">
     <Header></Header>
-    <div class="bg-red-700 m-6">
-      <p>Body</p>
+    <div class="bg-red-300 flex flex-wrap mx-24">
+      <div class="bg-red-700 w-96 m-6">
+        <div class="card bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Coke Zero</h2>
+            <p>A healthier alternative to regular coke</p>
+            <div class="card-actions justify-between mt-4">
+              <select class="select select-primary">
+                <option disabled selected>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+              <button class="btn btn-primary">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
