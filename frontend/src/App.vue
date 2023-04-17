9dfa190a5c785addc7377c5549502dcb18a3bc10
roycehoe <royceinbox@hotmail.com>
2023-04-17 20:29:57 +0800

    chore: Integrate tailwind into project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,9 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="w-24 bg-red-300">
+    <p>Hello world</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
