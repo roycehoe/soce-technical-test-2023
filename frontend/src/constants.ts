12d2e33501c47ba1f0ddcb307e8f16e5a5235338
roycehoe <royceinbox@hotmail.com>
2023-04-18 22:03:30 +0800

    feat: Create env file to toggle between production and development env for frontend

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,9 @@
+const envFile = import.meta.env as EnvVariables;
+
+interface EnvVariables {
+  VITE_BASE_URL?: string;
+}
+
 export const DEFAULT_PRICE_DECIMAL_PLACES = 2;
-export const BASE_URL = process.env.BASE_URL || "api/";
+
+export const BASE_URL = envFile.VITE_BASE_URL || "api/";
