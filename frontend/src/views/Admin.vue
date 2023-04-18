2c5f075af83571f74dad7f54bfd293b9da7ac227
roycehoe <royceinbox@hotmail.com>
2023-04-18 17:52:29 +0800

    feat: Integrate creation of new items between frontend and backend

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from "vue";
-import { ShopItemIn, useItem } from "../composables/useItem";
+import { ShopItemIn, ShopItemOut, useItem } from "../composables/useItem";
 import { useItems } from "../composables/useItems";
 import { DEFAULT_PRICE_DECIMAL_PLACES } from "../constants";
 
@@ -8,6 +8,12 @@ const { getItems } = useItems();
 const { createItem, updateItem, deleteItem, isLoading } = useItem();
 
 const shopItems = ref([] as ShopItemIn[]);
+const newItem = ref({
+  name: "",
+  quantity: 0,
+  price: 0,
+  description: "",
+} as ShopItemOut);
 
 async function fetchShopItemData() {
   const { ok: isSuccessful, val: response } = await getItems();
@@ -16,6 +22,15 @@ async function fetchShopItemData() {
     return;
   }
   console.log(response);
+}
+
+function resetNewItemForm(): void {
+  newItem.value = {
+    name: "",
+    quantity: 0,
+    price: 0,
+    description: "",
+  };
 }
 
 onBeforeMount(fetchShopItemData);
@@ -75,41 +90,62 @@ onBeforeMount(fetchShopItemData);
           </div>
         </div>
       </div>
-      <div class="w-96 m-6 curent-store--new-item">
-        <div class="card bg-base-100 shadow-xl current-store--card">
-          <div class="card-body">
-            <input
-              type="text"
-              placeholder="Name"
-              class="input input-ghost w-full max-w-xs p-0 card-title"
-            />
-            <input
-              type="text"
-              placeholder="Description"
-              class="input input-ghost w-full max-w-xs p-0"
-            />
-            <div class="divider m-0"></div>
-            <p class="card-title block">
-              $
+      <form
+        @submit.prevent="
+          async (event) => {
+            await createItem(newItem);
+            await fetchShopItemData();
+            resetNewItemForm();
+          }
+        "
+      >
+        <div class="w-96 m-6 curent-store--new-item">
+          <div class="card bg-base-100 shadow-xl current-store--card">
+            <div class="card-body">
               <input
                 type="text"
-                placeholder="Price"
-                class="input input-bordered w-16 p-0 text-center"
+                placeholder="Name"
+                class="input input-ghost w-full max-w-xs p-0 card-title"
+                v-model="newItem.name"
+                required
               />
-            </p>
-            <div class="card-actions justify-between mt-4">
               <input
                 type="text"
-                placeholder="Qty"
-                class="input input-bordered w-12 p-0 text-center"
+                placeholder="Description"
+                class="input input-ghost w-full max-w-xs p-0"
+                v-model="newItem.description"
+                required
               />
-              <div class="admin-mod-selection">
-                <button class="btn btn-primary mx-2">Create</button>
+              <div class="divider m-0"></div>
+              <p class="card-title block">
+                $
+                <input
+                  type="number"
+                  step="0.01"
+                  placeholder="Price"
+                  class="input input-bordered w-16 p-0 text-center"
+                  v-model="newItem.price"
+                  required
+                />
+              </p>
+              <div class="card-actions justify-between mt-4">
+                <input
+                  type="number"
+                  placeholder="Qty"
+                  class="input input-bordered w-12 p-0 text-center"
+                  v-model="newItem.quantity"
+                  required
+                />
+                <div class="admin-mod-selection">
+                  <button type="submit" class="btn btn-primary mx-2">
+                    Create
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
