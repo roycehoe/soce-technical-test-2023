ac503506fdf4a6f9e453df1875c7fbed3b8bb345
roycehoe <royceinbox@hotmail.com>
2023-04-18 18:50:41 +0800

    refactor: Move current item form into a separate component to ensure that each current item form only changes the current item data

--- frontend/src/views/Admin/Index.vue
+++ frontend/src/views/Admin/Index.vue
@@ -2,8 +2,8 @@
 import { onBeforeMount } from "vue";
 import { useItem } from "../../composables/useItem";
 import { currentItems, useItems } from "../../composables/useItems";
-import { DEFAULT_PRICE_DECIMAL_PLACES } from "../../constants";
-import NewItem from "./NewItem.vue";
+import CurrentItemForm from "./CurrentItemForm.vue";
+import NewItem from "./NewItemForm.vue";
 
 const { getItems, updateCurrentItems } = useItems();
 const { createItem, updateItem, deleteItem, isLoading } = useItem();
@@ -14,57 +14,12 @@ onBeforeMount(updateCurrentItems);
 <template>
   <div class="w-screen h-screen flex flex-col">
     <div class="flex flex-wrap mx-24 text-center current-store--group">
-      <div v-for="item in currentItems" class="w-96 m-6 curent-store--cards">
-        <div class="card bg-base-100 shadow-xl">
-          <div class="card-body">
-            <input
-              type="text"
-              placeholder="Name"
-              class="input input-ghost p-0 card-title text-center"
-              :value="item.name"
-            />
-            <input
-              type="text"
-              placeholder="Description"
-              class="input input-ghost w-full max-w-xs p-0 text-center"
-              :value="item.description"
-            />
-            <div class="divider m-0"></div>
-            <p class="card-title block">
-              $
-              <input
-                type="text"
-                placeholder="Price"
-                class="input input-bordered w-16 p-0 text-center"
-                :value="item.price.toFixed(DEFAULT_PRICE_DECIMAL_PLACES)"
-              />
-            </p>
-            <div class="card-actions justify-between mt-4">
-              <input
-                type="text"
-                placeholder="Qty"
-                class="input input-bordered w-12 p-0 text-center"
-                :value="item.quantity"
-              />
-              <div class="admin-mod-selection">
-                <button
-                  :disabled="isLoading"
-                  @click="
-                    async (event) => {
-                      await deleteItem(item.id);
-                      await updateCurrentItems();
-                    }
-                  "
-                  class="btn btn-primary mx-2"
-                >
-                  Delete
-                </button>
-                <button class="btn btn-primary mx-2">Save</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CurrentItemForm
+        v-for="item in currentItems"
+        class="w-96 m-6 curent-store--cards"
+        :key="item.id"
+        :item="item"
+      ></CurrentItemForm>
       <NewItem></NewItem>
     </div>
   </div>
