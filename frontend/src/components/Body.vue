fd25707990cf6a0afff38c74af2a39c191e90b98
roycehoe <royceinbox@hotmail.com>
2023-04-18 18:03:24 +0800

    refactor: Move new item form to separate component

--- frontend/src/components/Body.vue
+++ frontend/src/components/Body.vue
@@ -4,7 +4,8 @@ import {
   currentBodyComponent,
 } from "../composables/useBodyComponent";
 import { client } from "../services";
-import Admin from "../views/Admin.vue";
+// import Admin from "../views/Admin/Index.vue";
+import Admin from "../views/Admin/Index.vue";
 import Shop from "../views/Shop.vue";
 
 async function test() {
